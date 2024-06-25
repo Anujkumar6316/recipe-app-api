26d02d94b3d751b68d3cb9188a06a3cabfc983e0
Anuj kumar <anujkumar63166316@gmail.com>
2024-06-25 21:41:36 +0530

    db configuration

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add(self):
+        res = calc.add(6,4)
+        self.assertTrue(10, res)
+
+    def test_subtract(self):
+        res = calc.subtract(10,15)
+        self.assertTrue(res, 5)
